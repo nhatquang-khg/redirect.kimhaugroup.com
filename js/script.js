288361e2b58919544707c0d220b1bfc63d2d0d51
nhatquang-khg <nhatquang.khg@gmail.com>
2024-11-10 20:56:18 +0700

    Initial commit with redirect page

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Sử dụng setTimeout để chuyển hướng sau 4 giây
+setTimeout(function() {
+    window.location.href = "https://zalo.me/g/dygfga105"; // Thay thế link nhóm Zalo của bạn
+}, 4000); // Thời gian chờ là 4000ms (4 giây)
