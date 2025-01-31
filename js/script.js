92364600f307a086a77b29964522d126f30bbf6c
nhatquang-khg <nhatquang.khg@gmail.com>
2025-01-31 13:23:55 +0700

    Change Background

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ const urlParams = new URLSearchParams(window.location.search);
 const ref = urlParams.get('ref');
 
 // Link nhóm Zalo gốc của bạn
-let zaloLink = "https://zalo.me/g/lzlqwx001";
+let youtubeLink = "https://www.youtube.com/";
 
 // Nếu có giá trị 'ref', thêm vào link Zalo
 if (ref) {
-    zaloLink += `?ref=${ref}`;
+    youtubeLink += `?ref=${ref}`;
 }
 
 // Chuyển hướng sau khi video kết thúc hoặc theo thời gian chờ
 setTimeout(function() {
-    window.location.href = zaloLink;
-}, 4000); // 4000ms (4 giây)
+    window.location.href = youtubeLink;
+}, 100); // 4000ms (4 giây)
